b9e024736bbee652a973a90730dee5338c117b3d
kv-2 <str_token@mail.ru>
2019-07-29 19:38:11 +0300

    add data from model to view

--- app.rb
+++ app.rb
@@ -1,5 +1,8 @@
-require 'sinatra'
+require 'sinatra'# ruby app.rb
+require 'movie'# запуск: ruby -I lib app.rb
 
 get ('/movies') do
+  @movie = Movie.new
+  @movie.title = "Fuzzy"
   erb :index
 end
