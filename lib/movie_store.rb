4ce053191b389b5c1ffc33c01c515b4ff508d160
kv-2 <str_token@mail.ru>
2019-07-30 16:12:51 +0300

    add class to save movie with id in storage YAML::Store

---
+++ lib/movie_store.rb
@@ -0,0 +1,22 @@
+require 'yaml/store'
+
+class MovieStore
+
+  def initialize(file_name)
+    # создаёт хранилище
+    @store = YAML::Store.new(file_name)
+  end
+
+  # сохраняет объект Movie в хранилище
+  def save(movie)
+    @store.transaction do
+      # присваивает id
+      unless movie.id
+        highest_id = @store.roots.max || 0
+        movie.id = highest_id + 1
+      end
+      # объект сохраняется с id
+      @store[movie.id] = movie
+    end
+  end
+end
